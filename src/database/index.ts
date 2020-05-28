d8ddd5d15324ebd519714e05ceaefa5ac5b15658
ribeirogab <gblosr@gmail.com>
2020-05-28 14:10:35 -0300

    fix: resolve error in heroku deploy migrations

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,10 @@
 import { createConnection, getConnectionOptions } from 'typeorm';
 import * as PostgressConnectionStringParser from 'pg-connection-string';
 
+import User from '../models/User';
+import Task from '../models/Task';
+import Update from '../models/Update';
+
 async function connect(): Promise<void> {
   const connectionOptions = await getConnectionOptions();
 
@@ -16,18 +20,32 @@ async function connect(): Promise<void> {
       username: configDatabaseOptions.user,
       password: configDatabaseOptions.password,
       database: configDatabaseOptions.database,
-      entities: ['./dist/models/*.js'],
-      migrations: ['./dist/database/migrations/*.js'],
+      entities: [Task, Update, User],
+      migrations: ['./dist/database/migrations/*.js}'],
       cli: { migrationsDir: './dist/database/migrations' },
       synchronize: true,
-      extra: {
-        ssl: true,
-      },
+      // extra: {
+      //   ssl: true,
+      // },
     });
   }
 
   const connection = await createConnection(connectionOptions);
   await connection.runMigrations();
+
+  await connection
+    .createQueryBuilder()
+    .insert()
+    .into(User)
+    .values([
+      {
+        username: process.env.ADMIN_USERNAME,
+        name: process.env.ADMIN_NAME,
+        password: process.env.ADMIN_PASSWORD,
+        permission: 'administrator',
+      },
+    ])
+    .execute();
 }
 
 connect();
