152b7a40cea361c15411b7ff601e05f1d2012521
ribeirogab <gblosr@gmail.com>
2020-06-03 12:57:42 -0300

    feat: show, index, put and delete updates

--- src/routes/updates.routes.ts
+++ src/routes/updates.routes.ts
@@ -12,9 +12,18 @@ updatesRouter.use(ensureAuthenticated);
 updatesRouter.get('/', async (req, res) => {
   const updatesRepository = getRepository(Update);
 
-  const updates = await updatesRepository.find();
+  const updates = await updatesRepository.find({ order: { version: 'DESC' } });
 
   return res.json(updates);
+});
+
+updatesRouter.get('/:updateId', async (req, res) => {
+  const { updateId } = req.params;
+  const updatesRepository = getRepository(Update);
+
+  const update = await updatesRepository.findOne(updateId);
+
+  return res.json(update);
 });
 
 updatesRouter.post('/', async (req, res) => {
@@ -31,6 +40,27 @@ updatesRouter.post('/', async (req, res) => {
   await updatesRepository.save(update);
 
   return res.json(update);
+});
+
+updatesRouter.put('/:updateId', async (req, res) => {
+  const { updateId } = req.params;
+  const { version, title, description } = req.body;
+  const updatesRepository = getRepository(Update);
+
+  await updatesRepository.update(updateId, { version, title, description });
+
+  const update = await updatesRepository.findOne(updateId);
+
+  return res.json(update);
+});
+
+updatesRouter.delete('/:updateId', async (req, res) => {
+  const { updateId } = req.params;
+  const updatesRepository = getRepository(Update);
+
+  await updatesRepository.delete(updateId);
+
+  return res.status(204).send();
 });
 
 export default updatesRouter;
